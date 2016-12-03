b954905ad928456a4d1bf23634be873a32b8e68d
sarrainfo <sarra_hellal@hotmail.com>
2016-12-03 19:18:03 +0100

    Add files via upload

--- Tries/src/com/algav/HybridesTries/Ajout.java
+++ Tries/src/com/algav/HybridesTries/Ajout.java
@@ -1,8 +1,13 @@
 package com.algav.HybridesTries;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Scanner;
+
+import com.algav.HybridesTries.Exceptions.InvalidCharactereException;
 
 public class Ajout {
 	
@@ -50,6 +55,13 @@ public class Ajout {
 			
 		}
 	}
+	
+	
+	//apres l'ajout d'un mot, reequilibrage
+	public static HybridesTries ajoutStringEquilibrage(String mot,HybridesTries h){
+		HybridesTries a =ajoutString(mot, h);
+		return equilibrage(a);
+	}
 	public static HybridesTries ajoutText(String s, HybridesTries h)  {
 		HybridesTries newH = h;
 		try
@@ -70,6 +82,31 @@ public class Ajout {
 		
 		return newH;
 	}
+	
+	//apres chaque ajout d'un mot dans l'arbre on equilibre
+	public static HybridesTries ajoutTextEquilibrage(String s, HybridesTries h)  {
+		HybridesTries newH = h;
+		try
+		{
+			String[] splitArray = s.split(" ");
+			for (int i = 0 ; i < splitArray.length; i++){
+				if(verifie(splitArray[i])){
+				newH = ajoutStringEquilibrage(splitArray[i],newH);
+				
+				}
+			}
+		}catch (StringIndexOutOfBoundsException e){
+			System.out.println("retirer les espaces en trop");
+			
+		} catch (InvalidCharactereException e) {
+			System.out.println("les characteres entrer ne sont pas conforme");
+			e.printStackTrace();
+		}
+		
+		return newH;
+	}
+	
+	
 	public static boolean verifie(String mot) throws InvalidCharactereException{
 		for(int i = 0; i < mot.length();i++){
 			if(mot.charAt(i)<0 || mot.charAt(i)> 128)
@@ -125,14 +162,14 @@ public class Ajout {
 		}
 		return nbMots;
 	}
-	
+	//liste les mots d enotre arbre par ordre alphabetique 
 	public static ArrayList<String> liste(HybridesTries h){
 		ArrayList<String> lesmots = new ArrayList<String>();
 		listeMots(h,lesmots,"");
 		return lesmots;
 		
 	}
-	
+	// utiliser comme fonction intermediaire
 	public static void listeMots(HybridesTries h,ArrayList<String> lesMots,
 			String mot){
 		String chars = mot;
@@ -141,11 +178,14 @@ public class Ajout {
 			
 				listeMots(h.getInf(),lesMots,chars);
 				chars2 = chars + h.getRacine().getKey();
-				listeMots(h.getEq(),lesMots,chars2);
-				//chars+=h.getRacine().getKey();
 				if(h.getRacine().getValue() instanceof ValueNonVide){
 					lesMots.add(chars2);
-					}
+				}
+				listeMots(h.getEq(),lesMots,chars2);
+				//chars+=h.getRacine().getKey();
+				/*if(h.getRacine().getValue() instanceof ValueNonVide){
+					lesMots.add(chars2);
+					}*/
 						
 				listeMots(h.getSup(), lesMots,chars);
 						
@@ -239,7 +279,8 @@ public class Ajout {
 		}
 		return nb;		
 	}
-	
+	//insere l'arbre h dans l'arbre h au bon endroit
+	//utilise uniquement pour la fonction supression
 	public static HybridesTries inserer(HybridesTries h, HybridesTries g){
 		char c = h.getRacine().getKey();
 		
@@ -266,14 +307,6 @@ public class Ajout {
 				HybridesTries g = new HybridesTries(h.getInf(),h.getEq(),
 						suprimer(h.getSup(),mot,taille),h.getRacine());
 				
-				// s'il reste des caractere a retiré de l'arbre
-				/*if(taille > mot.length() && g.getEq().isVide()){
-					HybridesTries nv = inserer(g.getInf(),g.getSup().getInf());
-					return new HybridesTries(g.getSup().getInf(),
-							g.getSup().getEq(),
-							nv,
-							g.getSup().getRacine());
-					}*/
 					return g;
 				}
 				
@@ -291,11 +324,15 @@ public class Ajout {
 					}
 					if(!h.getEq().isVide()){
 						Value vide = new ValueVide();
-						h.getRacine().setValue(vide);
+						//h.getRacine().setValue(vide);
+						Node racine = new Node(h.getRacine().getKey(), vide);
 						return new HybridesTries(h.getInf(),h.getEq(),
-								h.getSup(),h.getRacine());
+								h.getSup(),racine);
 					}
 					if(h.getSup().isVide()){
+						/*return new HybridesTries(
+								h.getInf().getInf(),h.getInf().getEq(),
+								h.getInf().getSup(),h.getInf().getRacine());*/
 						return h.getInf();
 					}
 					HybridesTries nv = inserer(h.getInf(),h.getSup().getInf());
@@ -340,7 +377,106 @@ public class Ajout {
 			}
 		return suprimer(h,mot, mot.length());
 	}
+	//supprime un mot puis reequilibre l'arbre
+	public static HybridesTries supressionEqulibrage(HybridesTries h, String mot){
+		HybridesTries a = supression(h,mot);
+		return equilibrage(a);
+	}
 	
+	public static HybridesTries rotationDroite(HybridesTries a){
+		HybridesTries filsDroit = new HybridesTries(a.getInf().getSup(),
+				a.getEq(),a.getSup(), a.getRacine());
+		HybridesTries filsGauche = a.getInf().getInf();
+		HybridesTries filsEq = a.getInf().getEq();
+		HybridesTries newArbre = new HybridesTries(filsGauche,
+				filsEq,filsDroit,a.getInf().getRacine());
+		return newArbre;
+	}
+	
+	public static HybridesTries rotationGauche(HybridesTries a){
+		HybridesTries filsGauche = new HybridesTries(a.getInf()
+				, a.getEq(), a.getSup().getInf(), a.getRacine());
+		HybridesTries filsEq = a.getSup().getEq();
+		HybridesTries filsDroit = a.getSup().getSup();
+		HybridesTries newArbre =
+				new HybridesTries(filsGauche, filsEq, filsDroit, a.getSup().getRacine());
+		return newArbre;
+		
+	}
+	//calcule la pseudo hauteur de l'arbre 
+	public static int hauteurEqu(HybridesTries  h, boolean b ){
+		int nb = 0;
+		if(h.isVide())
+			return 0;
+		if( b == false){
+			
+			nb = 1 + Math.max(Math.max(hauteurEqu(h.getInf(),false),
+					hauteurEqu(h.getSup(),false)), 
+					hauteurEqu(h.getEq(),true));
+			return nb;
+		}
+		else {
+			nb = Math.max(Math.max(hauteurEqu(h.getInf(),false),
+					hauteurEqu(h.getSup(),false)), 
+					hauteurEqu(h.getEq(),true));
+			return nb;
+			
+		}
+		
+	}
+	public static int hauteurEquilibrage(HybridesTries h){
+		return hauteurEqu(h,false);
+	}
+	
+	//calcule la difrerence de hauteur entre le fils gauche et le fils droit
+	public static int  delta(HybridesTries a){
+		return hauteurEquilibrage(a.getSup())-hauteurEquilibrage(a.getInf());
+		
+	}
+	
+	public static HybridesTries equilibrage(HybridesTries h){
+		if( !h.isVide()){
+			HybridesTries inf = equilibrage(h.getInf());
+			HybridesTries eq = equilibrage(h.getEq());
+			HybridesTries sup = equilibrage(h.getSup());
+			// si arbre est deseqilibre
+			if(delta(h) !=  0){
+				// si l'arbre gauche est plus lourd que le droit
+				if( delta(h) < -1 ){
+					if( !inf.isVide() && delta(inf)< 0  ){
+						HybridesTries rd = rotationDroite(h);
+						return rd;
+					}
+					else {
+						HybridesTries rgd = rotationGauche(inf);
+						HybridesTries newArbre = new HybridesTries(rgd, h.getEq()
+								, h.getSup(), h.getRacine());
+						 rgd = rotationDroite(newArbre);
+						 return rgd;
+					}
+				}
+				else {
+					if(delta(h) > 1){
+						if(!sup.isVide() && delta(sup) > 0){
+							HybridesTries rg = rotationGauche(h);
+							return rg;
+						}
+						else {
+							HybridesTries rdg = rotationDroite(sup);
+							HybridesTries newArbre = new HybridesTries(h.getInf(),
+									h.getEq(),rdg , h.getRacine());
+							rdg = rotationGauche(newArbre);
+							return rdg;
+						}
+					}
+				}
+					
+			}
+			return h;
+		}
+		return h ;
+		
+	}
 }
 //https://algo.infoprepa.epita.fr//index.php/Epita:Algo:Cours:Info-Sup:Arbres_de_recherche
 	
